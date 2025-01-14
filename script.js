e90aacd7589d647256f05f1e4fd8f13184c69b43
FIRST_NAME LAST_NAME <woodies32005@gmail.com>
2025-01-14 13:34:15 -0600

    PRACTICE,NOT GETTING IT

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const bands = [
+  { Name: "Pink Floyd", Genre: "Rock", yearFormed: 1972, active: false },
+
+  { Name: "The Who", Genre: "Rock", yearFormed: 1969, active: false },
+
+  { Name: "Heart", Genre: "Rock", yearFormed: 1975, active: true },
+];
+
+function render(bands) {
+  const container = document.getElementById("music");
+  container.innerHTML = "";
+
+  for (let i = 0; i < bands.length; i++) 
+{
+    const band = bands[i];
+    let backgroundColor;
+
+    if (band.active) {
+      backgroundColor = green;
+    } else {
+      backgroundColor = red;
+    }
+  }
+  const card = `<div class="card" style="background-color: ${backgroundColor}"width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${bands.Name}</li>
+    <li class="list-group-item">${bands.Genre}</li>
+    <li class="list-group-item"> ${bands.yearFormed}</li>
+    <li class = "active-true-false">${bands.active}
+  </ul>
+</div>`;
+
+  container.innerHTML += card;
+   }
+  
+render(bands);
