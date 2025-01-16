bfd913b4cd04060b30307bad62eb5ad71b4619f4
FIRST_NAME LAST_NAME <woodies32005@gmail.com>
2025-01-15 21:42:57 -0600

    additional html

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function render(bands) {
       backgroundColor = "red";
     }
 
-    const card = `<div class="card" style="background-color: ${backgroundColor}"width: 18rem;">
+    const card = `<div class="card" style="background-color: ${backgroundColor}"width: 22rem;">
   <ul class="list-group -flush - list-unstyled">
     <li class="list-group-item">${band.Name}</li>
     <li class="list-group-item">${band.Genre}</li>
