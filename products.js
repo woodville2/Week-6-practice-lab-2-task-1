5609c6d45b2056e32a9daf80a097a5b720cbb707
FIRST_NAME LAST_NAME <woodies32005@gmail.com>
2025-01-18 00:39:10 -0600

    added images

--- products.js
+++ products.js
@@ -7,7 +7,7 @@ const products = [
     price: 3.99,
     available: true,
     image:
-      "https://unsplash.com/photos/a-glass-of-orange-juice-sitting-on-top-of-a-book-kFRC5gPX4CY",
+      "https://images.unsplash.com/photo-1650460069032-3c410224fe55?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlJTIwanVpY2V8ZW58MHx8MHx8fDA%3D",
   },
   {
     Name: "Bacon",
@@ -15,7 +15,7 @@ const products = [
     price: 6.99,
     available: true,
     image:
-      "https://www.istockphoto.com/photo/sizzling-bacon-frying-in-a-pan-gm472093755-32091588?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbacon&utm_medium=affiliate&utm_source=unsplash&utm_term=bacon%3A%3A%3A",
+      "https://media.istockphoto.com/id/472093755/photo/sizzling-bacon-frying-in-a-pan.webp?a=1&b=1&s=612x612&w=0&k=20&c=o3eEyJv3w9YDlvjugTSfBM4O6yKv5rNBvSzE1NAva8o=",
   },
   {
     Name: "Greek Yogurt",
@@ -23,7 +23,7 @@ const products = [
     price: 4.99,
     available: false,
     image:
-      "greek/ yogurthttps://unsplash.com/photos/red-and-white-ice-cream-on-clear-glass-container-l3lsro9TlYA.png",
+      "https://images.unsplash.com/photo-1618798513386-fedeb5c30d39?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z3JlZWslMjB5b2d1cnR8ZW58MHx8MHx8fDA%3D",
   },
 ];
 
@@ -40,7 +40,7 @@ function render(products) {
     }
 
     card = `<div class="card" style="width: 18rem;">
-  <img src= ${product.image}" class = "card-img-top" alt="${product.Name}">
+  <img src= "${product.image}" class = "card-img-top" alt="${product.Name}">
   <div class="card-body">
     <h5 class="card-title">${product.Name}</h5>
     <p class="card-text">${product.category}</p>
