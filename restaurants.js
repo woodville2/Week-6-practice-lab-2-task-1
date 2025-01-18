2c43cc3a78408973eefe15a5ed40f943ac992d62
FIRST_NAME LAST_NAME <woodies32005@gmail.com>
2025-01-17 20:20:50 -0600

    added js file

--- restaurants.js
+++ restaurants.js
@@ -1,1 +1,15 @@
 'use strict'
+const myFood= [
+
+    {Name:"Steak and Bake", 
+        cuisine:"American", 
+        location:"Houston Texas", rating: 5,}
+
+     {Name:"Cool Scoops", 
+        cuisine:"Dessert", 
+        location:"Boise Idaho", rating: 3,}
+];
+
+function render (myFood){
+    const container = document.getElementById ("food")
+}
