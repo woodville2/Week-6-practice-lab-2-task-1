470b0fa41ebb5e5822ffb71f4daa47d9a027d3b0
FIRST_NAME LAST_NAME <woodies32005@gmail.com>
2025-01-18 00:23:05 -0600

    images

--- products.js
+++ products.js
@@ -6,10 +6,25 @@ const products = [
     category: "Juice",
     price: 3.99,
     available: true,
-    image: "images/oj.png",
+    image:
+      "https://unsplash.com/photos/a-glass-of-orange-juice-sitting-on-top-of-a-book-kFRC5gPX4CY",
   },
-  { Name: "Bacon", category: "Meat", price: 6.99, available: true },
-  { Name: "Greek Yogurt", category: "Dairy", price: 4.99, available: false },
+  {
+    Name: "Bacon",
+    category: "Meat",
+    price: 6.99,
+    available: true,
+    image:
+      "https://www.istockphoto.com/photo/sizzling-bacon-frying-in-a-pan-gm472093755-32091588?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbacon&utm_medium=affiliate&utm_source=unsplash&utm_term=bacon%3A%3A%3A",
+  },
+  {
+    Name: "Greek Yogurt",
+    category: "Dairy",
+    price: 4.99,
+    available: false,
+    image:
+      "greek/ yogurthttps://unsplash.com/photos/red-and-white-ice-cream-on-clear-glass-container-l3lsro9TlYA.png",
+  },
 ];
 
 function render(products) {
@@ -25,7 +40,7 @@ function render(products) {
     }
 
     card = `<div class="card" style="width: 18rem;">
-  <img src="https://via.placeholder.com/150" class = "card-img-top" alt="placeholder">
+  <img src= ${product.image}" class = "card-img-top" alt="${product.Name}">
   <div class="card-body">
     <h5 class="card-title">${product.Name}</h5>
     <p class="card-text">${product.category}</p>
